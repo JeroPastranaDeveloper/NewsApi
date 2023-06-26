b28fd1c78274f8b7b39f809b5791a0a0e0d3cfa3
JeroPastranaDeveloper <jeropastrana.developer@gmail.com>
2023-06-26 10:58:35 +0200

    Done SplashScreen

--- app/src/main/java/com/example/newsapi/MainActivity.kt
+++ app/src/main/java/com/example/newsapi/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.newsapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initViews()
         setContentView(R.layout.activity_main)
+
+    }
+
+    private fun initViews() {
+        installSplashScreen()
     }
 }
