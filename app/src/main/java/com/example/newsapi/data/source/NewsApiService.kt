b73420db419c78b34a929e1564cc4e4b08214b39
JeroPastranaDeveloper <jeropastrana.developer@gmail.com>
2023-06-26 11:52:19 +0200

    Created data source layer

--- app/src/main/java/com/example/newsapi/data/source/NewsApiService.kt
+++ app/src/main/java/com/example/newsapi/data/source/NewsApiService.kt
@@ -1,4 +1,14 @@
 package com.example.newsapi.data.source
 
+import com.example.newsapi.data.model.New
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.Response
+
 interface NewsApiService {
+    @GET("top-headlines")
+    suspend fun getTopHeadlines(
+        @Query("country") country: String,
+        @Query("apiKey") apiKey: String
+    ): Response<New>
 }
--- app/src/main/java/com/example/newsapi/data/source/RetrofitInstance.kt
+++ app/src/main/java/com/example/newsapi/data/source/RetrofitInstance.kt
@@ -1,4 +1,15 @@
 package com.example.newsapi.data.source
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitInstance {
+    private const val BASE_URL = "https://newsapi.org/v2/"
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val newsApiService: NewsApiService = retrofit.create(NewsApiService::class.java)
 }
